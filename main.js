763567b521e3b19394ec30b10bc5525ff6f4a96d
Omoor <86531601+Omoorion@users.noreply.github.com>
2022-03-14 20:46:51 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["drive!", "power!", "hungry!", "DEVOUR!"];
+const textArray = ["Ough", "Bruh", "OMAGOD", "*Boom*", "*KrabLegs*"];
 const typingDelay = 100;
 const erasingDelay = 75;
 const newTextDelay = 1000; // Delay between current and next text
@@ -13,15 +13,6 @@ function type() {
     if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
     typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
     charIndex++;
-    if(textArrayIndex == 2){
-        document.getElementById("sentence").innerText = "We stay"
-    }
-    else if(textArrayIndex==3){
-        document.getElementById("sentence").innerText = "We"
-    }
-    else{
-        document.getElementById("sentence").innerText = "It's about"
-    }
     setTimeout(type, typingDelay);
   } 
   else {
