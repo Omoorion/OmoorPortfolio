7f6aa4481586c6fd8b73c8e4802f48c048c3068b
Omoorion <86531601+Omoorion@users.noreply.github.com>
2022-03-14 16:35:12 +0200

    balls

---
+++ main.js
@@ -0,0 +1,50 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["drive!", "power!", "hungry!", "DEVOUR!"];
+const typingDelay = 100;
+const erasingDelay = 75;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    if(textArrayIndex == 2){
+        document.getElementById("sentence").innerText = "We stay"
+    }
+    else if(textArrayIndex==3){
+        document.getElementById("sentence").innerText = "We"
+    }
+    else{
+        document.getElementById("sentence").innerText = "It's about"
+    }
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+    if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+    else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
